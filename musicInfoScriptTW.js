221c7d9643196dc92824c47bb7c37a3044d8b63a
Trevor Hamilton <tjh1003@sru.edu>
2017-06-04 20:45:09 -0400

    Twitch support

--- musicInfoScriptTW.js
+++ musicInfoScriptTW.js
@@ -1,98 +1,167 @@
 var oldTitle;
+var oldArtist;
+var oldAlbum;
+var oldAlbumArt;
 var oldPos;
+var oldDur;
+var oldLiked;
+var oldRepeat;
+var oldState;
+
 var ws;
 var connected = false;
 var reconnect;
+var sendData;
 
-function open()
-{
-    try {
-        var url = "ws://127.0.0.1:8974/";
-        ws = new WebSocket(url);
-        ws.onopen = onOpen;
-        ws.onclose = onClose;
-        ws.onmessage = onMessage;
-        ws.onerror = onError;
+function open() {
+	try {
+		var url = "ws://127.0.0.1:8974/";
+		ws = new WebSocket(url);
+		ws.onopen = onOpen;
+		ws.onclose = onClose;
+		ws.onmessage = onMessage;
+		ws.onerror = onError;
 
-        console.log("Opening websocket");
-    }
-    catch (error)
-    {
-        console.log("Error:" + error);
-    }
+		console.log("Opening websocket");
+	}
+	catch (error) {
+		console.log("Error:" + error);
+	}
 }
 
 var onOpen = function() {
-    console.log("Opened websocket");
-    connected = true;
-    clearTimeout(reconnect);
-    dataCheck();
+	console.log("Opened websocket");
+	connected = true;
+	ws.send("PLAYER:TWITCH");
+	//@TODO Possibly send all know data right away on open
+	sendData = setInterval(function() {
+		dataCheck();
+	}, 500);
 };
 
 var onClose = function() {
-    console.log("Closed websocket");
-    connected = false;
-    reconnect = setTimeout(function(){ open(); }, 5000);
+	console.log("Closed websocket");
+	connected = false;
+	clearInterval(sendData);
+	reconnect = setTimeout(function() {
+		open();
+	}, 5000);
 };
 
 var onMessage = function(event) {
-    console.log("Message received:" + event.data);
+	if (event.data.toLowerCase() == "playpause") {
+		var a = document.getElementsByClassName("js-control-playpause-button")[0];
+		var e = document.createEvent('MouseEvents');
+		e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		a.dispatchEvent(e);
+	}
 };
 
 var onError = function(event) {
-    if(typeof event.data != 'undefined') {
-        console.log("Websocket Error:" + event.data);
-    }
+	if (typeof event.data != 'undefined') {
+		console.log("Websocket Error:" + event.data);
+	}
 };
 
-function dataCheck()
-{
-    try
-    {
-        //Contains both the title and the album art
+function dataCheck() {
+	try {
+		if (document.getElementsByClassName("js-card__title").length > 0) {
+			var newTitle = document.getElementsByClassName("js-card__title")[0].innerText;
+			if (newTitle != oldTitle) {
+				oldTitle = newTitle;
 
-        newTitle = document.getElementById("currently-playing-title").title;
-        newPos = document.getElementById("time_container_current").innerHTML;
+				if (newTitle.indexOf(" • ") > 0) {
+					ws.send("TITLE:" + newTitle.substring(0, newTitle.indexOf(" • ")));
+				}
+				else {
+					ws.send("TITLE:" + newTitle);
+				}
+			}
 
 
-        if(newTitle != oldTitle || newPos != oldPos)
-        {
-            oldTitle = newTitle;
-            oldPos = newPos;
+			var newArtist = document.getElementsByClassName("cn-bar__displayname")[0].innerText;
+			if (newArtist != oldArtist) {
+				oldArtist = newArtist;
+				ws.send("ARTIST:" + newArtist);
+			}
 
-            title = newTitle;
-            artist = document.getElementById("player-artist").innerHTML;
-            album = document.getElementsByClassName("player-album")[0].innerHTML;
-            //Only contains album art thumbnail not full sized
-            albumArt =  document.getElementById("playerBarArt").src;
+			var newAlbum = document.getElementsByClassName("js-card__info")[0].children[0].innerText;
+			if (newAlbum != oldAlbum) {
+				oldAlbum = newAlbum;
+				ws.send("ALBUM:" + newAlbum);
+			}
 
-            position = newPos;
-            duration = document.getElementById("time_container_duration").innerHTML;
+			var newAlbumArt = document.getElementsByClassName("cn-metabar__boxart")[0].children[0].src;
+			if (newAlbumArt != oldAlbumArt) {
+				oldAlbumArt = newAlbumArt;
+				ws.send("ALBUMART:" + newAlbumArt.replace("-138x190", ""));
+			}
 
-            liked = document.getElementsByClassName("rating-container materialThumbs")[0].children[0].title;
-            disliked = document.getElementsByClassName("rating-container materialThumbs")[0].children[1].title;
+			//If not live
+			if (document.getElementById("js-player-seek").children.length > 0) {
+				var newDur = document.getElementsByClassName("player-seek__time--total")[0].innerText;
+				if (newDur != oldDur) {
+					oldDur = newDur;
+					ws.send("DURATION:" + newDur);
+				}
 
-            //Note this may get info right when using podcasts
-            //.children[0 & 6].title is back 30 seconds and forward 30 seconds
-            //.children[2 & 4].title are previous and next
-            repeat = document.getElementsByClassName("material-player-middle")[0].children[1].title;
-            shuffle = document.getElementsByClassName("material-player-middle")[0].children[5].title;
-            status = document.getElementsByClassName("material-player-middle")[0].children[3].title;
+				var newPos = document.getElementsByClassName("player-seek__time")[0].innerText;
+				if (newPos != oldPos) {
+					oldPos = newPos;
+					ws.send("POSITION:" + newPos);
+				}
+			}
+			else {
+				//@TODO Look at twitch api integration to current stream durration
+				var newDur = "0:00";
+				if (newDur != oldDur) {
+					oldDur = newDur;
+					ws.send("DURATION:" + newDur);
+				}
 
-            if(connected) {
-                ws.send(title);
-                ws.send(artist);
-                ws.send(album);
-                ws.send(status);
+				var newPos = "0:00";
+				if (newPos != oldPos) {
+					oldPos = newPos;
+					ws.send("POSITION:" + newPos);
+				}
+			}
 
-            }
-        }
-        setTimeout(dataCheck, 500);
-    }
-    catch(err)
-    {
-        setTimeout(dataCheck, 1000);
-    }
+
+			var newLiked = document.getElementsByClassName("follow-button")[0].innerText;
+			if (newLiked != oldLiked) {
+				oldLiked = newLiked;
+
+				var following = 0;
+				if (newLiked != "Follow") {
+					following = 5;
+				}
+				ws.send("RATING:" + following);
+			}
+
+      //Twitch does not have a way to tell which button is showing so just check position of pause button
+			var stateCheck = document.getElementsByClassName("js-pause-button")[0].getBoundingClientRect().top;
+      var newState = 2;
+      if (stateCheck > 0) {
+        newState = 1;
+      }
+			if (newState != oldState) {
+				oldState = newState;
+
+				ws.send("STATE:" + newState);
+			}
+		}
+		else {
+			//@TODO Decide on if/how to tell it to reset data/ignore this one
+			//Send playback as stopped
+			var newState = 0;
+			if (newState != oldState) {
+				oldState = newState;
+				ws.send("STATE:" + newState);
+			}
+		}
+	}
+	catch (e) {
+		ws.send("Error:" + e);
+	}
 }
-
 open();
