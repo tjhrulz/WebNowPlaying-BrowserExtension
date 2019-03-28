0cc6ec7c36451d5ff98ba1e6bd2e9c1dbc302b77
Trevor Hamilton <tjhrulz@gmail.com>
2019-03-28 17:23:57 -0400

    Tidal support fixed
    You will have to try harder than that next to time stop me. Fixes #25

--- Websites/Tidal.js
+++ Websites/Tidal.js
@@ -1,6 +1,8 @@
 //Adds support for Tidal
 /*global init createNewMusicInfo createNewMusicEventHandler convertTimeToString capitalize*/
 
+var element = null;
+
 function setup()
 {
 	var tidalInfoHandler = createNewMusicInfo();
@@ -12,59 +14,82 @@ function setup()
 
 	tidalInfoHandler.readyCheck = function()
 	{
-		return document.getElementsByClassName("now-playing__metadata__title").length > 0 &&
-			document.getElementsByClassName("now-playing__metadata__title")[0].innerText.length > 0 &&
-			document.getElementsByClassName("info-box__table").length > 0;
+		if (document.getElementsByTagName('audio').length > 0)
+		{
+			for (var i = 0; i < document.getElementsByTagName('audio').length; i++)
+			{
+				if (document.getElementsByTagName('audio')[i].duration > 0)
+				{
+					element = document.getElementsByTagName('audio')[i];
+					break;
+				}
+			}
+		}
+
+		return element !== null &&
+			document.querySelector('[class^="mediaInformation"]').children.length > 0 &&
+			document.querySelector('[class^="mediaInformation"]').children[0].innerText.length > 0;
 	};
 
 	tidalInfoHandler.state = function()
 	{
-		return document.getElementsByClassName("play-controls__main-button")[0].className.includes("paused") ? 2 : 1;
+		return element.paused ? 2 : 1;
 	};
 	tidalInfoHandler.title = function()
 	{
-		return document.getElementsByClassName("now-playing__metadata__title")[0].innerText;
+		return document.querySelector('[class^="mediaInformation"]').children[0].innerText;
 	};
 	tidalInfoHandler.artist = function()
 	{
-		return document.getElementsByClassName("now-playing__metadata__artist")[0].innerText;
+		return document.querySelector('[class^="mediaInformation"]').children[1].innerText;
 	};
 	tidalInfoHandler.album = function()
 	{
-		return document.getElementsByClassName("info-box__table")[0].children[0].children[2].children[1].innerText.replace("From ", "");
+		return document.querySelector('[class^="mediaInformation"]').children[2].children[1].innerText;
 	};
 	tidalInfoHandler.cover = function()
 	{
-		return document.getElementsByClassName("js-footer-player-image")[0].src.replace("320x320", "1280x1280");
+		//Capture small src image
+		var tempIMG = document.querySelector('[class^="mediaImagery"]').children[0].src;
+		//Remove size and replace it with max size before returning
+		return tempIMG.substring(0, tempIMG.lastIndexOf("/")) + "/1280x1280.jpg";
 	};
-	tidalInfoHandler.durationString = function()
+	tidalInfoHandler.duration = function()
 	{
-		return document.getElementsByClassName("js-duration")[0].innerText;
+		return element.duration;
 	};
-	tidalInfoHandler.positionString = function()
+	tidalInfoHandler.position = function()
 	{
-		return document.getElementsByClassName("js-progress")[0].innerText;
+		return element.currentTime;
 	};
 	tidalInfoHandler.volume = function()
 	{
-		return parseInt(document.getElementsByClassName("ui-slider-handle")[0].style.bottom) / 100;
+		return parseInt(document.querySelector('[class^="volumeSlider"]').getAttribute("aria-valuenow")) / 100;
 	};
-	tidalInfoHandler.rating = null;
+	tidalInfoHandler.rating = function()
+	{
+		if (document.querySelector('[class^="mediaActions"]').children[1].className.includes("favorite"))
+		{
+			return 5;
+		}
+		return 0;
+	};
 	tidalInfoHandler.repeat = function()
 	{
-		if (document.getElementsByClassName("play-controls__repeat")[0].className.includes("active"))
+		if (document.querySelector('[class^="repeatStateIcon"]').className.baseVal.includes("all"))
 		{
-			if (document.getElementsByClassName("icon-Playback_repeatOnce").length > 0)
-			{
-				return 2;
-			}
 			return 1;
+		}
+		if (document.querySelector('[class^="repeatStateIcon"]').className.baseVal.includes("once"))
+		{
+			return 2;
 		}
 		return 0;
 	};
 	tidalInfoHandler.shuffle = function()
 	{
-		if (document.getElementsByClassName("play-controls__shuffle")[0].className.includes("active"))
+		//Icon for shuffle button is goofy, they seem to have an oversite in their code and it only has a class when active
+		if (document.querySelector('[class^="shuffleIconActive"]') !== null)
 		{
 			return 1;
 		}
@@ -77,108 +102,83 @@ function setup()
 	//Define custom check logic to make sure you are not trying to update info when nothing is playing
 	tidalEventHandler.readyCheck = function()
 	{
-		return document.getElementsByClassName("now-playing__metadata__title").length > 0 &&
-			document.getElementsByClassName("now-playing__metadata__title")[0].innerText.length > 0 &&
-			document.getElementsByClassName("info-box__table").length > 0;
+		if (document.getElementsByTagName('audio').length > 0)
+		{
+			for (var i = 0; i < document.getElementsByTagName('audio').length; i++)
+			{
+				if (document.getElementsByTagName('audio')[i].duration > 0)
+				{
+					element = document.getElementsByTagName('audio')[i];
+					break;
+				}
+			}
+		}
+
+		return element !== null &&
+			document.querySelector('[class^="mediaInformation"]').children.length > 0 &&
+			document.querySelector('[class^="mediaInformation"]').children[0].innerText.length > 0;
 	};
 
 	tidalEventHandler.playpause = function()
 	{
-		if (document.getElementsByClassName("play-controls__main-button--paused").length > 0)
-		{
-			//Click play button
-			document.getElementsByClassName("play-controls__main-button")[0].children[0].click();
-		}
-		else
-		{
-			//Click pause button
-			document.getElementsByClassName("play-controls__main-button")[0].children[1].click();
-		}
+		document.querySelector('[class^="playbackToggle"]').click();
 	};
 	tidalEventHandler.next = function()
 	{
-		document.getElementsByClassName("play-controls__next")[0].click();
+		document.querySelector('[class^="playbackControls"]').children[3].click();
 	};
 	tidalEventHandler.previous = function()
 	{
-		document.getElementsByClassName("play-controls__previous")[0].click();
+		document.querySelector('[class^="playbackControls"]').children[1].click();
 	};
-	tidalEventHandler.progress = function(progress)
+	tidalEventHandler.progressSeconds = function(position)
 	{
-		var loc = document.getElementsByClassName("progressbar__interaction-layer")[0].getBoundingClientRect();
-		progress *= loc.width;
-
-		var a = document.getElementsByClassName("progressbar__interaction-layer")[0];
-		var e = document.createEvent('MouseEvents');
-		e.initMouseEvent('mousedown', true, true, window, 1,
-			screenX + loc.left + progress, screenY + loc.top + loc.height / 2,
-			loc.left + progress, loc.top + loc.height / 2,
-			false, false, false, false, 0, null);
-		a.dispatchEvent(e);
-		e.initMouseEvent('mouseup', true, true, window, 1,
-			screenX + loc.left + progress, screenY + loc.top + loc.height / 2,
-			loc.left + progress, loc.top + loc.height / 2,
-			false, false, false, false, 0, null);
-		a.dispatchEvent(e);
+		element.currentTime = position;
 	};
 	tidalEventHandler.volume = function(volume)
 	{
-		var a = document.getElementsByClassName("js-volume-status")[0];
-		var e = document.createEvent('MouseEvents');
-		e.initMouseEvent('mouseover', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-		a.dispatchEvent(e);
-		e.initMouseEvent('mousemove', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-		a.dispatchEvent(e);
-
-		var counter = 0;
-		var volumeReadyTest = setInterval(function()
+		if (element.muted && volume > 0)
 		{
-			if (document.getElementsByClassName("show-slider").length > 0)
-			{
-				clearInterval(volumeReadyTest);
-				var loc = document.getElementsByClassName("volume-slider")[0].getBoundingClientRect();
-				volume *= loc.height;
-
-				a = document.getElementsByClassName("volume-slider")[0];
-				e = document.createEvent('MouseEvents');
-				//As much as I hate hard coded stuff for some reason the click is always of by 5, no idea where it comes from but it is always exactly 5
-				e.initMouseEvent('mousedown', true, true, window, 1,
-					screenX + loc.left + loc.width / 2, screenY + loc.bottom - volume + 5,
-					loc.left + loc.width / 2, loc.bottom - volume,
-					false, false, false, false, 0, null);
-				a.dispatchEvent(e);
-				e.initMouseEvent('mouseup', true, true, window, 1,
-					screenX + loc.left + loc.width / 2, screenY + loc.bottom - volume + 5,
-					loc.left + loc.width / 2, loc.bottom - volume,
-					false, false, false, false, 0, null);
-				a.dispatchEvent(e);
-
-				a = document.getElementsByClassName("js-volume-status")[0];
-				e = document.createEvent('MouseEvents');
-				e.initMouseEvent('mouseout', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-				a.dispatchEvent(e);
-			}
-			else
-			{
-				counter++;
-				if (counter > 10)
-				{
-					clearInterval(volumeReadyTest);
-				}
-			}
-		}, 25);
+			element.muted = false;
+		}
+		else if (volume == 0)
+		{
+			element.muted = true;
+		}
+		element.volume = volume;
 	};
 	tidalEventHandler.repeat = function()
 	{
-		document.getElementsByClassName("play-controls__repeat")[0].click();
+		document.querySelector('[class^="playbackControls"]').children[4].click();
 	};
 	tidalEventHandler.shuffle = function()
 	{
-		document.getElementsByClassName("play-controls__shuffle")[0].click();
+		document.querySelector('[class^="playbackControls"]').children[0].click();
 	};
-	tidalEventHandler.toggleThumbsUp = null;
+	tidalEventHandler.toggleThumbsUp = function()
+	{
+		document.querySelector('[class^="mediaActions"]').children[1].click();
+	};
 	tidalEventHandler.toggleThumbsDown = null;
-	tidalEventHandler.rating = null;
+	tidalEventHandler.rating = function(rating)
+	{
+		//Check if thumbs has two paths, if it does not then it is active
+		if (rating > 3)
+		{
+			//If thumbs up is not active
+			if (!document.querySelector('[class^="mediaActions"]').children[1].className.includes("favorite"))
+			{
+				document.querySelector('[class^="mediaActions"]').children[1].click();
+			}
+		}
+		else
+		{
+			if (document.querySelector('[class^="mediaActions"]').children[1].className.includes("favorite"))
+			{
+				document.querySelector('[class^="mediaActions"]').children[1].click();
+			}
+		}
+	};
 }
 
 
