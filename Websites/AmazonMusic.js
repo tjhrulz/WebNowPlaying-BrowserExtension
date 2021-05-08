0818aa5b0831878701c6f008a5bb7f10e1dd3a25
Trevor Hamilton <hamilton.tj@outlook.com>
2021-05-08 14:37:29 -0400

    Updated amazon music support
    Added support for the new version of Amazon Music. Fixes #65

--- Websites/AmazonMusic.js
+++ Websites/AmazonMusic.js
@@ -1,301 +1,159 @@
 //Adds support for Amazon Music
 /*global init createNewMusicInfo createNewMusicEventHandler convertTimeToString capitalize*/
 
-var lastVolume = null;
-
-var lastBigCover = "";
-var lastSmallCover = "";
-var useBigCover = false;
-
-//Amazon's new volume for some stupid reason only goes up to 93.75, They should be grateful I was even willing to support it
-var AMAZONVOLUMESCALE = 93;
+//Amazon does not always have their volume visible and thus we need to cache what the last known value was
+//We assume it is 1 since that is the default value and what most people are going to have it at
+var lastKnownVolume = 1;
 
 function setup()
 {
-	var amzmInfoHandler = createNewMusicInfo();
+	var amznInfoHandler = createNewMusicInfo();
 
-	amzmInfoHandler.player = function()
+	amznInfoHandler.player = function()
 	{
 		return "Amazon Music";
 	};
 
-	amzmInfoHandler.readyCheck = function()
+	amznInfoHandler.readyCheck = function()
 	{
-		return document.getElementsByClassName("trackTitle").length > 0;
+		return document.getElementsByTagName("music-horizontal-item").length > 0 && document.getElementById("transport") !== null;
 	};
 
-	amzmInfoHandler.state = function()
+	amznInfoHandler.state = function()
 	{
-		return document.getElementsByClassName("playerIconPause").length > 0 ? 1 : 2;
+		//If icon is pause then we are currently playing
+		return document.getElementById("transport").children[1].children[2].getAttribute("icon-name").includes("pause") ? 1 : 2;
 	};
-	amzmInfoHandler.title = function()
+	amznInfoHandler.title = function()
 	{
-		return document.getElementsByClassName("trackTitle")[0].innerText;
+		return document.getElementById("transport").children[0].children[0].getAttribute("primary-text");
 	};
-	amzmInfoHandler.artist = function()
+	amznInfoHandler.artist = function()
 	{
-		return document.getElementsByClassName("trackArtist")[0].innerText;
+		return document.getElementById("transport").children[0].children[0].getAttribute("secondary-text");
 	};
-	amzmInfoHandler.album = function()
+	amznInfoHandler.album = function()
 	{
-		return document.getElementsByClassName("trackSourceLink")[0].children[0].children[0].innerText;
+		return document.getElementById("transport").children[0].children[0].getAttribute("secondary-text-2");
 	};
-	amzmInfoHandler.cover = function()
+	amznInfoHandler.cover = function()
 	{
-		//Amazon did some optimization on their end and now only loads (Or updated) the big image when it is visable
-		//Thus we need to figure out if it has been loaded and then once it has if it is from the same album as current album
-		var bigCover = document.getElementsByClassName("largeAlbumArtContainer")[0].children[0].style.backgroundImage.replace('url("', "").replace('")', "");
-		var smallCover = document.getElementsByClassName("trackAlbumArt")[0].children[0].children[0].children[0].src;
-
-		//If the small cover has changed then we know it is a new album art
-		if (smallCover !== lastSmallCover)
-		{
-			//If the big cover changed at the same time then we know it is also the same album art
-			if (bigCover !== lastBigCover)
-			{
-				lastSmallCover = smallCover;
-				lastBigCover = bigCover;
-				useBigCover = true;
-				return bigCover;
-			}
-
-			//If it has not changed it should be ignored as it is out of date
-			lastSmallCover = smallCover;
-			useBigCover = false;
-			return smallCover;
-		}
-
-		//If the big cover has changed then we know that is now visable and is also up to date
-		if (bigCover !== lastBigCover)
-		{
-			lastBigCover = bigCover;
-			useBigCover = true;
-			return bigCover;
-		}
-
-		if (useBigCover)
-		{
-			return bigCover;
-		}
-		return smallCover;
+		return document.getElementById("transport").children[0].children[0].getAttribute("image-src");
 
 	};
-	amzmInfoHandler.duration = function()
+	amznInfoHandler.duration = function()
 	{
-		//Amazon only gives time to end of the song, so use that to calculate
-		var timeFromEndString = document.getElementsByClassName("listViewDuration")[0].innerHTML.replace("-", "");
+		//Amazon does not give total time, just time into the song and time remaining. So we get the seconds of each and then add them together
+		//@NOTE Keep an eye on this in the future, they may add an option to show total time instead of time reamaining like other services
+		var timeFromEndString = document.getElementById("transport").lastElementChild.lastElementChild.children[1].innerText.replace("- ", "");
 		var timeFromEndSec = parseInt(timeFromEndString.substring(timeFromEndString.indexOf(":") + 1)) + parseInt(timeFromEndString.substring(0, timeFromEndString.indexOf(":"))) * 60;
-		var timeFromEndPercent = parseFloat(document.getElementsByClassName("sliderTrack scrubberTrack")[0].children[2].style.width.replace("%", ""));
-
-		return Math.round(timeFromEndSec / timeFromEndPercent * 100);
+		var timeFromBeginingString = document.getElementById("transport").lastElementChild.lastElementChild.children[0].innerText
+		var timeFromBeginingSec = parseInt(timeFromBeginingString.substring(timeFromBeginingString.indexOf(":") + 1)) + parseInt(timeFromBeginingString.substring(0, timeFromBeginingString.indexOf(":"))) * 60;
+		
+		return timeFromBeginingSec + timeFromEndSec;
 	};
-	amzmInfoHandler.position = function()
+	amznInfoHandler.position = function()
 	{
-		//Amazon only gives time to end of the song, so use that to calculate
-		var timeFromEndString = document.getElementsByClassName("listViewDuration")[0].innerHTML.replace("-", "");
-		var timeFromEndSec = parseInt(timeFromEndString.substring(timeFromEndString.indexOf(":") + 1)) + parseInt(timeFromEndString.substring(0, timeFromEndString.indexOf(":"))) * 60;
-		var timeFromEndPercent = parseFloat(document.getElementsByClassName("sliderTrack scrubberTrack")[0].children[2].style.width.replace("%", ""));
-
-		return Math.round(timeFromEndSec / timeFromEndPercent * parseFloat(document.getElementsByClassName("scrubberTrack")[0].children[0].style.width.replace("%", "")));
+		var timeFromBeginingString = document.getElementById("transport").lastElementChild.lastElementChild.children[0].innerText
+		var timeFromBeginingSec = parseInt(timeFromBeginingString.substring(timeFromBeginingString.indexOf(":") + 1)) + parseInt(timeFromBeginingString.substring(0, timeFromBeginingString.indexOf(":"))) * 60;
+		
+		return timeFromBeginingSec;
 	};
-	amzmInfoHandler.volume = function()
+	amznInfoHandler.volume = function()
 	{
-		if (document.getElementsByClassName("volumeSlider").length > 0)
+		if(document.getElementById("volume-range") !== null)
 		{
-			lastVolume = parseInt(document.getElementsByClassName("volumeSlider")[0].children[0].children[2].style.height) / AMAZONVOLUMESCALE;
+			lastKnownVolume = document.getElementById("volume-range").value;
 		}
-		//If we dont have a volume to return then return null
-		else if (lastVolume === null)
-		{
-			//Quickly open the volume record the result and close it
-			document.getElementsByClassName("volume")[0].click();
-			setTimeout(function()
-			{
-				lastVolume = parseInt(document.getElementsByClassName("volumeSlider")[0].children[0].children[2].style.height) / AMAZONVOLUMESCALE;
-				document.getElementsByClassName("volume")[0].click();
-			}, 40);
-		}
-
-		//Can be null if timeout has not fired yet
-		return lastVolume;
+		return lastKnownVolume;
 	};
-	amzmInfoHandler.rating = function()
+	amznInfoHandler.rating = function()
 	{
-		if (document.getElementsByClassName("thumbsUpButton").length > 0)
+		//Icon name is add when it is not in library, icon name is done otherwise.
+		//If it is in library assume liked
+		if (document.getElementById("transport").children[0].children[1].children[0].children[0].getAttribute("icon-name") === "done")
 		{
-			if (document.getElementsByClassName("thumbsUpButton")[0].className.includes(" on"))
-			{
-				return 5;
-			}
-			if (document.getElementsByClassName("thumbsDownButton")[0].className.includes(" on"))
-			{
-				return 1;
-			}
+			return 5;
 		}
 		return 0;
 	};
-	amzmInfoHandler.repeat = function()
+	//The new Amazon Music redesign has multiple encapsulation layers that abstract away my access to lower level elements so I sadly have to use aria labels
+	//Because of this repeat and shuffle may not work in all languages
+	amznInfoHandler.repeat = function()
 	{
-		if (document.getElementsByClassName("repeatButton").length > 0)
+		var state = document.getElementById("transport").children[1].children[0].getAttribute("aria-label");
+		
+		//Amazon labels everything based of the next type, since I have no way to work off besides label this will have to do
+		if (state.indexOf("One") != -1)
 		{
-			return document.getElementsByClassName("repeatButton")[0].getAttribute("aria-checked") == "true" ? 1 : 0;
+			return 1;
+		}
+		else if (state.indexOf("Off") != -1)
+		{
+			return 2;
 		}
 		return 0;
 	};
-	amzmInfoHandler.shuffle = function()
+	amznInfoHandler.shuffle = function()
 	{
-		if (document.getElementsByClassName("shuffleButton").length > 0)
+		//If the aria label is turn off shuffle then it must be on
+		if (document.getElementById("transport").children[1].children[4].getAttribute("aria-label").includes("Off"))
 		{
-			return document.getElementsByClassName("shuffleButton")[0].getAttribute("aria-checked") == "true" ? 1 : 0;
+			return 1;
 		}
-		return 1;
+		return 0;
 	};
 
 
-	var amzmEventHandler = createNewMusicEventHandler();
+	var amznEventHandler = createNewMusicEventHandler();
 
 	//Define custom check logic to make sure you are not trying to update info when nothing is playing
-	amzmEventHandler.readyCheck = function()
+	amznEventHandler.readyCheck = function()
 	{
-		return document.getElementsByClassName("trackTitle").length > 0;
+		return document.getElementsByTagName("music-horizontal-item").length > 0 && document.getElementById("transport") !== null;
 	};
 
-	amzmEventHandler.playpause = function()
+	amznEventHandler.playpause = function()
 	{
-		document.getElementsByClassName("playButton")[0].click();
+		document.getElementById("transport").children[1].children[2].click();
 	};
-	amzmEventHandler.next = function()
+	amznEventHandler.next = function()
 	{
-		document.getElementsByClassName("nextButton")[0].click();
+		document.getElementById("transport").children[1].children[3].click();
 	};
-	amzmEventHandler.previous = function()
+	amznEventHandler.previous = function()
 	{
-		document.getElementsByClassName("previousButton")[0].click();
+		document.getElementById("transport").children[1].children[1].click();
 	};
-	amzmEventHandler.progress = function(progress)
+	//Amazon now uses input type range for both their progress and volume
+	amznEventHandler.progress = null;
+	amznEventHandler.volume = null;
+	amznEventHandler.repeat = function()
 	{
-		var loc = document.getElementsByClassName("sliderTrack scrubberTrack")[0].getBoundingClientRect();
-		progress *= loc.width;
-
-		var a = document.getElementsByClassName("sliderTrack scrubberTrack")[0];
-		var e = document.createEvent('MouseEvents');
-		e.initMouseEvent('mousedown', true, true, window, 1,
-			screenX + loc.left + progress, screenY + loc.top + loc.height / 2,
-			loc.left + progress, loc.top + loc.height / 2,
-			false, false, false, false, 0, null);
-		a.dispatchEvent(e);
-		e.initMouseEvent('mouseup', true, true, window, 1,
-			screenX + loc.left + progress, screenY + loc.top + loc.height / 2,
-			loc.left + progress, loc.top + loc.height / 2,
-			false, false, false, false, 0, null);
-		a.dispatchEvent(e);
+		document.getElementById("transport").children[1].children[0].click()
 	};
-	amzmEventHandler.volume = function(volume)
+	amznEventHandler.shuffle = function()
 	{
-		////Amazon's volume is flipped
-		volume = 1 - volume;
-
-		if (document.getElementsByClassName("volumeSlider").length == 0)
+		document.getElementById("transport").children[1].children[4].click()
+	};
+	amznEventHandler.toggleThumbsUp = function()
+	{
+		document.getElementById("transport").children[0].children[1].children[0].children[0].click();
+	};
+	amznEventHandler.toggleThumbsDown = null;
+	amznEventHandler.rating = function(rating)
+	{
+		if (rating > 3)
 		{
-			document.getElementsByClassName("volume")[0].click();
-		}
-
-		var volumeReadyTest = setInterval(function()
-		{
-			if (document.getElementsByClassName("sliderTrack volumeTrack").length > 0)
+			//If not added to library
+			if (document.getElementById("transport").children[0].children[1].children[0].children[0].getAttribute("icon-name") === "add")
 			{
-				clearInterval(volumeReadyTest);
-				var loc = document.getElementsByClassName("sliderTrack volumeTrack")[0].getBoundingClientRect();
-				volume *= loc.height;
-
-				var a = document.getElementsByClassName("sliderTrack volumeTrack")[0];
-				var e = document.createEvent('MouseEvents');
-				e.initMouseEvent('mousedown', true, true, window, 1,
-					screenX + loc.left + loc.width / 2, screenY + loc.top + volume,
-					loc.left + loc.width / 2, loc.top + volume,
-					false, false, false, false, 0, null);
-				a.dispatchEvent(e);
-
-				//Amazon is dumb and does not read on mouse up but rather on mouse move :shrug:
-				e.initMouseEvent('mousemove', true, true, window, 0,
-					screenX + loc.left + loc.width / 2, screenY + loc.top + volume,
-					loc.left + loc.width / 2, loc.top + volume,
-					false, false, false, false, 0, null);
-				a.dispatchEvent(e);
-				e.initMouseEvent('mouseup', true, true, window, 1,
-					screenX + loc.left + loc.width / 2, screenY + loc.top + volume,
-					loc.left + loc.width / 2, loc.top + volume,
-					false, false, false, false, 0, null);
-				a.dispatchEvent(e);
-
-				//Because somehow if you don't leave it open for a moment it won't always stick *shrugs*
-				setTimeout(function()
-				{
-					document.getElementsByClassName("volume")[0].click();
-				}, 34);
+				document.getElementById("transport").children[0].children[1].children[0].children[0].click();
 			}
-		}, 33);
-	};
-	amzmEventHandler.repeat = function()
-	{
-		if (document.getElementsByClassName("repeatButton").length > 0)
-		{
-			document.getElementsByClassName("repeatButton")[0].click();
 		}
-	};
-	amzmEventHandler.shuffle = function()
-	{
-		if (document.getElementsByClassName("shuffleButton").length > 0)
+		else if (document.getElementById("transport").children[0].children[1].children[0].children[0].getAttribute("icon-name") === "done")
 		{
-			document.getElementsByClassName("shuffleButton")[0].click();
-		}
-	};
-	amzmEventHandler.toggleThumbsUp = function()
-	{
-		if (document.getElementsByClassName("thumbsUpButton").length > 0)
-		{
-			document.getElementsByClassName("thumbsUpButton")[0].click();
-		}
-	};
-	amzmEventHandler.toggleThumbsDown = function()
-	{
-		if (document.getElementsByClassName("thumbsDownButton").length > 0)
-		{
-			document.getElementsByClassName("thumbsDownButton")[0].click();
-		}
-	};
-	amzmEventHandler.rating = function(rating)
-	{
-		if (document.getElementsByClassName("thumbsUpButton").length > 0)
-		{
-			//Check if thumbs has two paths, if it does not then it is active
-			if (rating > 3)
-			{
-				//If thumbs up is not active
-				if (!document.getElementsByClassName("thumbsUpButton")[0].className.includes(" on"))
-				{
-					document.getElementsByClassName("thumbsUpButton")[0].click();
-				}
-			}
-			else if (rating < 3 && rating > 0)
-			{
-				//If thumbs down is not active
-				if (!document.getElementsByClassName("thumbsDownButton")[0].className.includes(" on"))
-				{
-					document.getElementsByClassName("thumbsDownButton")[0].click();
-				}
-			}
-			else
-			{
-				if (!document.getElementsByClassName("thumbsUpButton")[0].className.includes(" on"))
-				{
-					document.getElementsByClassName("thumbsUpButton")[0].click();
-				}
-				if (!document.getElementsByClassName("thumbsDownButton")[0].className.includes(" on"))
-				{
-					document.getElementsByClassName("thumbsDownButton")[0].click();
-				}
-			}
+			document.getElementById("transport").children[0].children[1].children[0].children[0].click();
 		}
 	};
 }
